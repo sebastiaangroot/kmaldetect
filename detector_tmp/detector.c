6ece6d735e381a4481030c37b20a691008e8dce3
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-06-10 13:46:29 +0200

    Deleted old detector directory

--- detector_tmp/detector.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-	printf("User-level malware detection application\n");
-	return 0;
-}
--- detector_tmp/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-extern void *malrealloc(void *ptr, size_t size);
-extern void *malcalloc(size_t num, size_t size);
-extern void *malmalloc(size_t size);
-
-#endif
