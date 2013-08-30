0d84354afdf1e6203728970799b3fa50822bcfbd
Sebastiaan Groot <sebastiaangroot@gmail.com>
2013-08-30 21:36:21 +0200

    Moved the scripts to a seperate "tools" directory

---
+++ tools/gen_syscall_table.ps1
@@ -0,0 +1,45 @@
+<#
+This script is used to generate a function for the sig_gen.sh script, translating a syscall name to a numeric ID.
+
+This script requires one source file from the Linux kernel.
+Infile: /arch/x86/include/asm/unistd_xx.h (xx = 32 / 64)
+
+The Outfile is the location of the generated bash file containing the function needed for sig_gen.sh.
+#>
+
+Param(
+	[string]$Infile,
+	[string]$Outfile
+)
+
+$content = Get-Content -Path $Infile
+New-Item -Path $Outfile -ItemType file -Force
+
+#Print out .sh style switch that translates a syscall name to an index number
+$sys_index = ""
+"function get_syscall_index" | Out-File -FilePath $Outfile -Append -Force
+"{" | Out-File -FilePath $Outfile -Append -Force
+"	case `$1 in" | Out-File -FilePath $Outfile -Append -Force
+for ($i = 0; $i -lt $content.Length; $i++)
+{
+	if ($content[$i].StartsWith("#define __NR_") -AND (-NOT ($content[$i+1].Contains("stub_"))))
+	{
+		$line_substr = $content[$i].Substring($content[$i].IndexOf("__NR_") + 5)
+		if ($line_substr.Contains("`t")) #Either substring on the first space or tab character
+		{
+			$sys_index = $content[$i].Clone()
+			$sys_index = $sys_index.Substring($sys_index.IndexOf("`t")).Replace("`t", "")
+			"		'" + $line_substr.Substring(0, $line_substr.IndexOf("	")) + "')" | Out-File -FilePath $Outfile -Append -Force
+		}
+		else
+		{
+			$sys_index = $content[$i].Clone()
+			$sys_index = $sys_index.Substring($sys_index.IndexOf(" ", 10)).Replace(" ", "")
+			"		'" + $line_substr.Substring(0, $line_substr.IndexOf(" ")) + "')" | Out-File -FilePath $Outfile -Append -Force
+		}
+		"			return $sys_index" | Out-File -FilePath $Outfile -Append -Force
+		"			;;" | Out-File -FilePath $Outfile -Append -Force
+	}
+}
+"	esac" | Out-File -FilePath $Outfile -Append -Force
+"}" | Out-File -FilePath $Outfile -Append -Force
