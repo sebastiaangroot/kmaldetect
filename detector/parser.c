3140d203282ff3b8bdf22badd3181832d345c2d7
sebastiaangroot <sebastiaangrooot@gmail.com>
2013-10-10 13:25:18 +0200

    Changed it so parsing progress is only displayed every 10%

--- detector/parser.c
+++ detector/parser.c
@@ -355,12 +355,21 @@ void tmp_syscall_parser(void)
 	int count = 0;
 	int percentage = 0;
 	int i;
+	int showedstatus = 0;
 	for (i = 0; i < tmp_syscalls_len; i++)
 	{
 		handle_input(&tmp_syscalls[i]);
 		count++;
 		percentage = (int)(((double)count / (double)tmp_syscalls_len) * 100);
-		printf("handle_input progress: %i%%\n", percentage);
+		if (percentage % 10 == 0 && percentage != 0 && !showedstatus)
+		{
+			showedstatus = 1;
+			printf("Parsing progress: %i%%\n", percentage);
+		}
+		else if (percentage % 10 != 0)
+		{
+			showedstatus = 0;
+		}
 	}
 }
 
