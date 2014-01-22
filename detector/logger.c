847917eb3f987c3620e60353d7d593884d65b078
Sebastiaan Groot <sebastiaangroot@gmail.com>
2014-01-22 09:43:52 +0100

    Replaced starting spaces with tabs

--- detector/logger.c
+++ detector/logger.c
@@ -9,12 +9,12 @@ extern int tm_states_len;
 
 struct list
 {
-    union {
-        pid_t *pids;
-        unsigned long *ulongs;
-    };
-    int p;
-    int alloc;
+	union {
+		pid_t *pids;
+		unsigned long *ulongs;
+	};
+	int p;
+	int alloc;
 };
 typedef struct list LIST;
 
@@ -24,65 +24,65 @@ LIST *pids = NULL;
 
 static void store_inode(unsigned long inode, int state)
 {
-    if (inodes[state].p >= inodes[state].alloc)
-    {
-        inodes[state].ulongs = malrealloc(inodes[state].ulongs, (inodes[state].alloc + MULTIPLIER) * sizeof(unsigned long));
-        inodes[state].alloc += MULTIPLIER;
-    }
-    inodes[state].ulongs[inodes[state].p] = inode;
-    inodes[state].p++;
+	if (inodes[state].p >= inodes[state].alloc)
+	{
+		inodes[state].ulongs = malrealloc(inodes[state].ulongs, (inodes[state].alloc + MULTIPLIER) * sizeof(unsigned long));
+		inodes[state].alloc += MULTIPLIER;
+	}
+	inodes[state].ulongs[inodes[state].p] = inode;
+	inodes[state].p++;
 }
 
 static void store_memloc(unsigned long memloc, int state)
 {
-    if (memlocs[state].p >= memlocs[state].alloc)
-    {
-        memlocs[state].ulongs = malrealloc(memlocs[state].ulongs, (memlocs[state].alloc + MULTIPLIER) * sizeof(unsigned long));
-        memlocs[state].alloc += MULTIPLIER;
-    }
-    memlocs[state].ulongs[memlocs[state].p] = memloc;
-    memlocs[state].p++;
+	if (memlocs[state].p >= memlocs[state].alloc)
+	{
+		memlocs[state].ulongs = malrealloc(memlocs[state].ulongs, (memlocs[state].alloc + MULTIPLIER) * sizeof(unsigned long));
+		memlocs[state].alloc += MULTIPLIER;
+	}
+	memlocs[state].ulongs[memlocs[state].p] = memloc;
+	memlocs[state].p++;
 }
 
 static void store_pid(pid_t pid, int state)
 {
-    if (pids[state].p >= pids[state].alloc)
-    {
-        pids[state].pids = malrealloc(pids[state].pids, (pids[state].alloc + MULTIPLIER) * sizeof(pid_t));
-        pids[state].alloc += MULTIPLIER;
-    }
-    pids[state].pids[pids[state].p] = pid;
-    pids[state].p++;
+	if (pids[state].p >= pids[state].alloc)
+	{
+		pids[state].pids = malrealloc(pids[state].pids, (pids[state].alloc + MULTIPLIER) * sizeof(pid_t));
+		pids[state].alloc += MULTIPLIER;
+	}
+	pids[state].pids[pids[state].p] = pid;
+	pids[state].p++;
 }
 
 void store_metadata(SYSCALL *sys, int state)
 {
-    store_inode(sys->inode, state);
-    store_memloc(sys->mem_loc, state);
-    store_pid(sys->pid, state);
+	store_inode(sys->inode, state);
+	store_memloc(sys->mem_loc, state);
+	store_pid(sys->pid, state);
 }
 
 void print_metadata(int state)
 {
-    int i;
+	int i;
 
-    printf("Inodes:\n");
-    for (i = 0; i < inodes[state].p; i++)
-    {
-        printf("\t%lu\n", inodes[state].ulongs[i]);
-    }
-    
-    printf("\nMemlocs:\n");
-    for (i = 0; i < memlocs[state].p; i++)
-    {
-        printf("\t%lu\n", memlocs[state].ulongs[i]);
-    }
-    
-    printf("\nFound pids:\n");
-    for (i = 0; i < pids[state].p; i++)
-    {
-        printf("\t%i\n", pids[state].pids[i]);
-    }
+	printf("Inodes:\n");
+	for (i = 0; i < inodes[state].p; i++)
+	{
+		printf("\t%lu\n", inodes[state].ulongs[i]);
+	}
+
+	printf("\nMemlocs:\n");
+	for (i = 0; i < memlocs[state].p; i++)
+	{
+		printf("\t%lu\n", memlocs[state].ulongs[i]);
+	}
+
+	printf("\nFound pids:\n");
+	for (i = 0; i < pids[state].p; i++)
+	{
+		printf("\t%i\n", pids[state].pids[i]);
+	}
 }
 
 struct counter
@@ -142,34 +142,34 @@ void print_metadata_very_verbose(void)
 
 void init_logger(void)
 {
-    int i;
+	int i;
 
-    inodes = malmalloc(tm_states_len * sizeof(LIST));
-    memlocs = malmalloc(tm_states_len * sizeof(LIST));
-    pids = malmalloc(tm_states_len * sizeof(LIST));
-    for (i = 0; i < tm_states_len; i++)
-    {
-        inodes[i].ulongs = malmalloc(MULTIPLIER * sizeof(unsigned long));
-        inodes[i].p = 0;
-        inodes[i].alloc = MULTIPLIER;
-        
-        memlocs[i].ulongs = malmalloc(MULTIPLIER * sizeof(unsigned long));
-        memlocs[i].p = 0;
-        memlocs[i].alloc = MULTIPLIER;
+	inodes = malmalloc(tm_states_len * sizeof(LIST));
+	memlocs = malmalloc(tm_states_len * sizeof(LIST));
+	pids = malmalloc(tm_states_len * sizeof(LIST));
+	for (i = 0; i < tm_states_len; i++)
+	{
+		inodes[i].ulongs = malmalloc(MULTIPLIER * sizeof(unsigned long));
+		inodes[i].p = 0;
+		inodes[i].alloc = MULTIPLIER;
 
-        pids[i].pids = malmalloc(MULTIPLIER * sizeof(pid_t));
-        pids[i].p = 0;
-        pids[i].alloc = MULTIPLIER;
-    }
+		memlocs[i].ulongs = malmalloc(MULTIPLIER * sizeof(unsigned long));
+		memlocs[i].p = 0;
+		memlocs[i].alloc = MULTIPLIER;
+
+		pids[i].pids = malmalloc(MULTIPLIER * sizeof(pid_t));
+		pids[i].p = 0;
+		pids[i].alloc = MULTIPLIER;
+	}
 }
 
 void flush_logger(void)
 {
-    int i;
-    for (i = 0; i < tm_states_len; i++)
-    {
-        inodes[i].p = 0;
-        memlocs[i].p = 0;
-        pids[i].p = 0;
-    }
+	int i;
+	for (i = 0; i < tm_states_len; i++)
+	{
+		inodes[i].p = 0;
+		memlocs[i].p = 0;
+		pids[i].p = 0;
+	}
 }
